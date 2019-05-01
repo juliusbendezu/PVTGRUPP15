59452bbbb5690c359a0007c3e77b55f2b3db5a80
Julius Bendezu <julius.bendezu@gmail.com>
2019-05-01 11:42:52 +0200

    Megacomit adding Android studio application

---
+++ android-app/app/src/test/java/com/dsv2019/pvt15/prepapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.dsv2019.pvt15.prepapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
