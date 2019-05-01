c9335799878cb9bcc85a820a156c1c2c69b71b55
Julius Bendezu <julius.bendezu@gmail.com>
2019-05-01 13:50:22 +0200

    Fixed theme

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/LoginActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/LoginActivity.java
@@ -1,9 +1,9 @@
 package com.dsv2019.pvt15.prepapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
