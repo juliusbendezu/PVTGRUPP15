1d2f5b817d9a979eea3841f310955e83bb17ebf6
Elsa B <bergman.elsa@gmail.com>
2019-05-05 22:15:18 +0200

    Maps Installed

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/MainActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.dsv2019.pvt15.prepapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
+    private ImageButton mapsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mapsButton = findViewById(R.id.mapsButton);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MapsActivity2.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
