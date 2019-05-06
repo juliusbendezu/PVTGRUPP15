bf0dd98c05b5e36deb3ff689a85662c9a62c25a1
Julius Bendezu <julius.bendezu@gmail.com>
2019-05-06 15:04:25 +0200

    Updated controller

--- prepup-server/src/main/java/com/example/demo/HelloRestController.java
+++ prepup-server/src/main/java/com/example/demo/HelloRestController.java
@@ -3,12 +3,17 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.*;
 
 
-
 @RestController
+@RequestMapping("/helloThere")
 public class HelloRestController {
 
-	@RequestMapping("/Hellothere")
+	@RequestMapping("/basic")
 	public String sayHello() {
 		return "Hello There!";
+	}
+	
+	@RequestMapping(value = "/name")
+	public String sayHello(@RequestParam(value = "name", required = true) String name) {
+		return "Hello There " + name;
 	}
 }
