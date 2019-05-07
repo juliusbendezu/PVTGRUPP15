301cb10452d78129d4b70d163fd0c85f1b9dceb3
Elsa B <bergman.elsa@gmail.com>
2019-05-07 14:32:00 +0200

    ?

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/MapsActivity2.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/MapsActivity2.java
@@ -9,8 +9,13 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.dsv2019.pvt15.prepapp.mapobject.BombShelter;
+import com.dsv2019.pvt15.prepapp.mapobject.MapObject;
+import com.dsv2019.pvt15.prepapp.mapobject.Position;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,6 +26,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
 
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback
 {
@@ -36,6 +43,11 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
 
+    private ImageButton vattenButton;
+    private ImageButton skyddButton;
+    private ImageButton akutButton;
+
+    ArrayList<MapObject> mapObjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -64,8 +76,65 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
             mMap.getUiSettings().setZoomControlsEnabled(true);
-
         }
+        initializeMarkers();
+
+
+    }
+
+    private void initializeMarkers(){
+
+        LatLng stockholm = new LatLng(59, 18);
+        mMap.addMarker(new MarkerOptions().position(stockholm).title("Marker in Sthlm"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(stockholm));
+
+        Position pos = new Position(59.5, 18.5);
+        BombShelter bm = new BombShelter(pos, "Hågavägen 95, 112 61 sthlm", 180);
+        mapObjects.add(bm);
+        Position posi = new Position(60, 19);
+        BombShelter bmm = new BombShelter(posi, "Hågavägen 95, 112 61 sthlm", 180);
+        mapObjects.add(bmm);
+        Position posit = new Position(59.3, 18.7);
+        BombShelter bmmm = new BombShelter(posit, "Hågavägen 95, 112 61 sthlm", 180);
+        mapObjects.add(bmmm);
+        Position positi = new Position(59.8, 18.9);
+        BombShelter bmmmm = new BombShelter(positi, "Hågavägen 95, 112 61 sthlm", 180);
+        mapObjects.add(bmmmm);
+
+        vattenButton = findViewById(R.id.vatten);
+        skyddButton = findViewById(R.id.skyddsrum);
+        akutButton = findViewById(R.id.akutmottagning);
+        vattenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // vad som ska hända om man klickar på vattenknapp
+            }
+        });
+
+        akutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // vad som ska hända om man klickar på akutmottagningsknapp
+            }
+        });
+
+        skyddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // vad som ska hända om man klickar på skyddsrum
+
+                for(MapObject mo :mapObjects){
+                    System.out.print(mapObjects);
+                    if (mo instanceof BombShelter) {
+                        LatLng marker = new LatLng(mo.getPos().getX(), mo.getPos().getY());
+                        mMap.addMarker(new MarkerOptions().position(marker).title(mo.toString()));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+                    }}
+            }
+
+
+        });
+
     }
 
     private void getDeviceLocation()
