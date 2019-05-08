d4ee318df750aef32d62b2955044275cee7d7692
Elsa B <bergman.elsa@gmail.com>
2019-05-08 14:02:40 +0200

    change in String values

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
@@ -1,16 +1,28 @@
 package com.dsv2019.pvt15.prepapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.dsv2019.pvt15.prepapp.R;
 
 public class TipsActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
+
+
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MapsActivity2.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
