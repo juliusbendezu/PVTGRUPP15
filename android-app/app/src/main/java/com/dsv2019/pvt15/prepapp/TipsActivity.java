41ab1516e0a4c51786de95733e79533c5dc529b9
Elsa B <bergman.elsa@gmail.com>
2019-05-08 14:28:47 +0200

    small fixes

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
@@ -3,24 +3,24 @@ package com.dsv2019.pvt15.prepapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 
-import com.dsv2019.pvt15.prepapp.R;
+
 
 public class TipsActivity extends Activity {
 
-
+private ImageButton homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
 
-
-        mapsButton.setOnClickListener(new View.OnClickListener() {
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), MapsActivity2.class);
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(startIntent);
             }
         });
