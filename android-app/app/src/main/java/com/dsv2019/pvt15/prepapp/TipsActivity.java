d418c2b0a86ce6cd093c7d33c468dbe6fa570d43
Elsa B <bergman.elsa@gmail.com>
2019-05-08 16:18:25 +0200

    Fixes in TipsView

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/TipsActivity.java
@@ -5,16 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
+import java.util.ArrayList;
+import com.dsv2019.pvt15.prepapp.tipsrelated.SingleTips;
 
 
 public class TipsActivity extends Activity {
 
-private ImageButton homeButton;
+    ArrayList<SingleTips> tipsList = new ArrayList<>();
+
+    private ImageButton homeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
+
+
 
         homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +29,9 @@ private ImageButton homeButton;
                 startActivity(startIntent);
             }
         });
+
+
+
+
     }
 }
--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/tipsrelated/SingleTips.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/tipsrelated/SingleTips.java
@@ -2,5 +2,28 @@ package com.dsv2019.pvt15.prepapp.tipsrelated;
 
 public class SingleTips   {
 
+    private String name;
+    private String description;
+    private int likes;
 
+    public SingleTips(String name, String description) {
+        this.description=description;
+        this.name=name;
+
+    }
+
+    public void setPopularity(int opinion){
+        if(opinion > 0){
+            likes ++;
+        }else{
+            likes--;
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        return name + " Description: " + description;
+    }
 }
