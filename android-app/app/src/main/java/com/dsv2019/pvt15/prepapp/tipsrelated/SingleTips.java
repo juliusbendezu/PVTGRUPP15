9ca1051e2d6a48d8bf83c4db1f1f75edff59ddac
Elsa B <bergman.elsa@gmail.com>
2019-05-14 10:38:34 +0200

    updated functionality in SingleTips

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/tipsrelated/SingleTips.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/tipsrelated/SingleTips.java
@@ -1,10 +1,14 @@
 package com.dsv2019.pvt15.prepapp.tipsrelated;
 
-public class SingleTips   {
+import com.dsv2019.pvt15.prepapp.models.NewsItem;
+
+import java.util.Objects;
+
+public class SingleTips implements Comparable<SingleTips>  {
 
     private String name;
     private String description;
-    private int likes;
+    private int likes = 0;
     private String creator;
     private int category;
     private int ID;
@@ -12,19 +16,61 @@ public class SingleTips   {
 
 
 
-    public SingleTips(String name, String description) {
+    public SingleTips(String name, String description, int category,String creator,int ID) {
         this.description=description;
         this.name=name;
-
+        this.ID=ID;
+        this.category=category;
+        this.creator=creator;
     }
 
-    public void setPopularity(int opinion){
+    public void setLikes(int opinion){
         if(opinion > 0){
             likes ++;
         }else{
             likes--;
         }
+    }
 
+    public String getName(){
+        return name;
+    }
+    public int getID(){
+        return ID;
+    }
+    public int getCategory(){
+        return category;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getCreator(){
+        return creator;
+    }
+    public int getLikes(){
+        return likes;
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SingleTips singleTips = (SingleTips) o;
+        return name.equals(singleTips.name) &&
+                description.equals(singleTips.description) &&
+                creator.equals(singleTips.creator) &&
+                ID==(singleTips.ID) &&
+                category==(singleTips.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID);
+    }
+
+    @Override
+    public int compareTo(SingleTips o) {
+        return likes.compareTo(o.likes);
     }
 
 
