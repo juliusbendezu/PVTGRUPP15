1f57741fbb37d9e4cffcfdce7fee452854aa1c8f
JWATSU <38113786+JWATSU@users.noreply.github.com>
2019-05-22 12:39:57 +0200

    Facebook integration ongoing work

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/LoginActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/LoginActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -14,6 +16,7 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -23,6 +26,8 @@ public class LoginActivity extends Activity
     private LoginButton mLoginButton;
     private CallbackManager mCallbackManager;
     private static final String EMAIL = "email";
+    private TextView idTv;
+    private TextView emailTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,26 +39,13 @@ public class LoginActivity extends Activity
         mLoginButton = findViewById(R.id.login_button);
 
         mLoginButton.setReadPermissions(Arrays.asList(EMAIL));
+        checkLoginStatus();
 
         mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>()
         {
             @Override
             public void onSuccess(LoginResult loginResult)
             {
-                AccessToken accessToken = loginResult.getAccessToken();
-                GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback()
-                {
-                    @Override
-                    public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse)
-                    {
-                        Display_data(jsonObject);
-
-                    }
-                });
-                Bundle bundle = new Bundle();
-                bundle.putString("fields", "email, id");
-                graphRequest.setParameters(bundle);
-                graphRequest.executeAsync();
 
             }
 
@@ -71,10 +63,72 @@ public class LoginActivity extends Activity
         });
     }
 
-    private void Display_data(JSONObject object)
+    AccessTokenTracker tokenTracker = new AccessTokenTracker()
     {
-        TextView textView = findViewById(R.id.email_login_text);
-        textView.setText(object.toString());
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
+        {
+
+            if (currentAccessToken == null)
+            {
+                idTv.setText(null);
+                emailTv.setText(null);
+                Toast.makeText(LoginActivity.this, "User logged out", Toast.LENGTH_SHORT).show();
+
+            }
+            else
+            {
+                loadUserProfile(currentAccessToken);
+            }
+
+        }
+    };
+
+    private void loadUserProfile(AccessToken newAccessToken)
+    {
+        GraphRequest graphRequest = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback()
+        {
+            @Override
+            public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse)
+            {
+                DisplayData(jsonObject);
+
+            }
+        });
+        Bundle bundle = new Bundle();
+        bundle.putString("fields", "email, id");
+        graphRequest.setParameters(bundle);
+        graphRequest.executeAsync();
+    }
+
+    private void DisplayData(JSONObject object)
+    {
+        try
+        {
+            String stringID = object.getString("id");
+            String email = object.getString("email");
+
+            idTv = findViewById(R.id.id_login_text);
+            idTv.setText(stringID);
+
+            emailTv = findViewById(R.id.email_login_text);
+            emailTv.setText(email);
+
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void checkLoginStatus()
+    {
+        if (AccessToken.getCurrentAccessToken() != null)
+        {
+            loadUserProfile(AccessToken.getCurrentAccessToken());
+
+        }
     }
 
     @Override
