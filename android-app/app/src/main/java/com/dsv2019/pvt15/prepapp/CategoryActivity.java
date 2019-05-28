bd5899330412f57f2a0b9f762d7ec55c26048191
Elsa B <bergman.elsa@gmail.com>
2019-05-28 11:02:18 +0200

    en ej färdig commit

--- android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/CategoryActivity.java
+++ android-app/app/src/main/java/com/dsv2019/pvt15/prepapp/CategoryActivity.java
@@ -1,8 +1,15 @@
 package com.dsv2019.pvt15.prepapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.ImageSpan;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,7 +31,6 @@ public class CategoryActivity extends Activity {
     private Button otherButton8;
     private ImageButton createNewTip;
     private ImageButton hamburger;
-
 
 
     @Override
@@ -161,12 +167,12 @@ public class CategoryActivity extends Activity {
 
     }
 
-    private void setHamburgerButton(){
+    private void setHamburgerButton() {
         hamburger = findViewById(R.id.tipsActivityHamburger);
         hamburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PopupMenu menu = new PopupMenu(CategoryActivity.this,v);
+                PopupMenu menu = new PopupMenu(CategoryActivity.this, v);
                 menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
@@ -183,5 +189,52 @@ public class CategoryActivity extends Activity {
         });
     }
 
+    /**
+     * Moves icons from the PopupMenu's MenuItems' icon fields into the menu title as a Spannable with the icon and title text.
+     */
+    public static void insertMenuItemIcons(Context context, PopupMenu popupMenu) {
+        Menu menu = popupMenu.getMenu();
+        if (hasIcon(menu)) {
+            for (int i = 0; i < menu.size(); i++) {
+                insertMenuItemIcon(context, menu.getItem(i));
+            }
+        }
+    }
 
+    /**
+     * @return true if the menu has at least one MenuItem with an icon.
+     */
+    private static boolean hasIcon(Menu menu) {
+        for (int i = 0; i < menu.size(); i++) {
+            if (menu.getItem(i).getIcon() != null) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Converts the given MenuItem's title into a Spannable containing both its icon and title.
+     */
+    private static void insertMenuItemIcon(Context context, MenuItem menuItem) {
+        Drawable icon = menuItem.getIcon();
+
+        // If there's no icon, we insert a transparent one to keep the title aligned with the items
+        // which do have icons.
+        if (icon == null) icon = new ColorDrawable(Color.TRANSPARENT);
+
+        int iconSize = context.getResources().getDimensionPixelSize(R.dimen.menu_item_icon_size);
+        icon.setBounds(0, 0, iconSize, iconSize);
+        ImageSpan imageSpan = new ImageSpan(icon);
+
+        // Add a space placeholder for the icon, before the title.
+        SpannableStringBuilder ssb = new SpannableStringBuilder("       " + menuItem.getTitle());
+
+        // Replace the space placeholder with the icon.
+        ssb.setSpan(imageSpan, 1, 2, 0);
+        menuItem.setTitle(ssb);
+        // Set the icon to null just in case, on some weird devices, they've customized Android to display
+        // the icon in the menu... we don't want two icons to appear.
+        menuItem.setIcon(null);
+
+
+    }
 }
